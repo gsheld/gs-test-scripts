e081cc11cdf5645bf23066d514e8cf4c9d558f68
Grant Sheldon <gsheld@gmail.com>
2016-04-28 09:44:54 -0700

    activating-layout-constraints-twice.playground

---
+++ swift/activating-layout-constraints-twice.playground/Contents.swift
@@ -0,0 +1,41 @@
+/**
+ * GOAL:
+ *    To determine if multiple calls to NSLayoutConstraint.activateConstraints() lead to a duplication of NSLayoutConstraint
+ *    instances associated with their respective views.
+ *
+ * FINDINGS: 
+ *    Multiple calls to NSLayoutConstraint.activateConstraints() does NOT lead to a duplication of NSLayoutConstraint instances
+ *    associated with their respective views.
+ */
+
+import UIKit
+
+let aView = UIView()
+let bView = UIView()
+
+let views = [ "aView": aView, "bView": bView ]
+bView.addSubview(aView)
+aView.translatesAutoresizingMaskIntoConstraints = false
+bView.translatesAutoresizingMaskIntoConstraints = false
+
+let constraints = NSLayoutConstraint.constraintsWithVisualFormat("H:|[aView]|", options: [], metrics: nil, views: views)
+
+// NOTE: Rm'ing both of these 2 calls has the expected effect of dropping the constraint count on `bView` to 0.
+NSLayoutConstraint.activateConstraints(constraints)
+NSLayoutConstraint.activateConstraints(constraints) // Does NOT duplicate constraints
+
+/*
+ * Prints 2 constraints corresponding to the visual formatting string activated above. Specifically:
+ *
+ * <NSLayoutConstraint:0x7f8fa0430180 H:|-(0)-[UIView:0x7f8fa0427900]   (Names: '|':UIView:0x7f8fa042e5c0 )>
+ * <NSLayoutConstraint:0x7f8fa04303f0 H:[UIView:0x7f8fa0427900]-(0)-|   (Names: '|':UIView:0x7f8fa042e5c0 )>
+ */
+for constraint in bView.constraints {
+  print(constraint)
+}
+
+
+// Prints 0 constraints. There are no constraints attached to the child view.
+for constraint in aView.constraints {
+  print(constraint)
+}
